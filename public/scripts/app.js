13d4d60f6f39dceaf1611e9f9effc85e0d63eb77
Anisa Thomas <anisathomas99@gmail.com>
2018-05-24 17:04:31 +0000

    Added refetch tweets on submission functionality

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,18 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 function loadTweets (){
-    $.ajax({
-          url: 'http://localhost:8080/tweets',
-          method: 'GET',
-          success: function (tweets) {
-            renderTweets(tweets);
-          }
-     });
-  }
-
+  $.ajax({
+      url: 'http://localhost:8080/tweets',
+      method: 'GET',
+      success: function (tweets) {
+        renderTweets(tweets);
+      }
+  });
+}
 
 $(document).ready(function () {
-
    $("form").on( "submit", function( event ) {
     event.preventDefault();
     var serializedData = $(this).serialize();
@@ -35,59 +33,26 @@ $(document).ready(function () {
       });
     }
   });
-
  loadTweets();
-
 });
 
 
-
-// The form should not be cleared
-// The form should not submit
-
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-
 function renderTweets(tweets) {
-  for (var i = 0; i < tweets.length; i++){
-    var $tweet = createTweetElement(tweets[i]);
+  $('.sectionOfTweets').empty()
+  //sort tweets b4 appending latest tweet
+  var sortedTweets = tweets.sort(function(a, b){
+    return b.created_at-a.created_at;
+  })
+  for (var i = 0; i < sortedTweets.length; i++){
+    var $tweet = createTweetElement(sortedTweets[i]);
     ($tweet).appendTo('.sectionOfTweets')
   }
+
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the section of tweets container
 }
+
 
 function createTweetElement(tweet) {
   var $tweet = $("<article>");
